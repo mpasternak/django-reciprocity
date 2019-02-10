f93d1ced2af159cb3b1df637ced627888b0b9f2a
Michał Pasternak <michal.dtz@gmail.com>
2019-02-10 20:18:24 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Django'
+]
+
+setup(
+    name='django_reciprocity',
+    version='0.0.1',
+    description="Interactive web apps with Django",
+    long_description=readme + '\n\n' + history,
+    author="Michał Pasternak",
+    author_email='michal.dtz@gmail.com',
+    url='https://github.com/mpasternak/django-reciprocity',
+    packages=[
+        'reciprocity',
+    ],
+    package_dir={'reciprocity': 'reciprocity'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
