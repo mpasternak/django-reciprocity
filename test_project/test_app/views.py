73c8cc78806b9d64b8149063f29a2b9726dfce5e
Michał Pasternak <michal.dtz@gmail.com>
2019-02-10 19:59:54 +0100

    Initial import

--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponseNotAllowed, HttpResponse
 from django.views.generic import TemplateView
+
+from .tasks import long_running_task
 
 
 class RootPage(TemplateView):
     template_name = "root.html"
-    pass
 
+
+def start_long_process(request):
+    if not request.is_ajax():
+        return HttpResponseNotAllowed()
+    long_running_task.delay(web_page_uuid=request.GET['uuid'])
+    return HttpResponse("{}")
