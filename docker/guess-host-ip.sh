ce2d078111ae855e90e18cb2f66d8716d6919117
Michał Pasternak <michal.dtz@gmail.com>
2019-02-12 12:15:03 +0100

    Host IP support for Linux

---
+++ docker/guess-host-ip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+export POSSIBLE_IP=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+
+if [ "x$POSSIBLE_IP" == "x" ]; then
+    export POSSIBLE_IP=$(getent hosts host.docker.internal | awk '{print $1 }')
+fi
+
+if [ "x$POSSIBLE_IP" == "x" ]; then
+    echo "host IP not available";
+    exit 1;
+fi
+
+echo "$POSSIBLE_IP docker.host" >> /etc/hosts
