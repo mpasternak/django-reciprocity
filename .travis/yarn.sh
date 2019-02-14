d58de59148dbf825fb3885ee3d7d75e0afa1a52c
Michał Pasternak <michal.dtz@gmail.com>
2019-02-14 22:08:10 +0100

    Fix build

---
+++ .travis/yarn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+
+main() {
+  setup_dependencies
+
+  echo "INFO:
+  Done! Finished setting up Travis-CI machine.
+  "
+}
+
+# Takes care of updating any dependencies that the
+# machine needs.
+setup_dependencies() {
+  echo "INFO:
+  Setting up dependencies.
+  "
+
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  
+  sudo apt-get update && sudo apt-get install -y yarn
+
+  yarn --version
+}
+
+main
