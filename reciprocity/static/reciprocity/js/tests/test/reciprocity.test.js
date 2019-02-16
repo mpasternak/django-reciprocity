5fc0368cd6c34dc2cc332b1f06df0444857350b9
Michał Pasternak <michal.dtz@gmail.com>
2019-02-16 20:40:13 +0100

    JS tests, as many as possible without hitting integration

--- reciprocity/static/reciprocity/js/tests/test/reciprocity.test.js
+++ reciprocity/static/reciprocity/js/tests/test/reciprocity.test.js
@@ -1,27 +1,50 @@
-QUnit.module('Test Module', {   
+QUnit.module('Test Module', {
     beforeEach: function () {
-        sinon.replace(jQuery.fn, 'loadTemplate',  sinon.stub());
-	iplweb.reciprocity.init("token", "prefix", "../../doesntmatter/");
+        sinon.replace(jQuery.fn, 'loadTemplate', sinon.stub());
+        iplweb.reciprocity.init("token", "prefix", "../../doesntmatter/");
+        iplweb.reciprocity.pushstream = sinon.createStubInstance(PushStream, {
+            connect: sinon.stub(),
+            addChannel: sinon.stub()
+        });
     },
     afterEach: function () {
-	sinon.restore();
+        sinon.restore();
     }
 });
 
-QUnit.test('check connect', function(assert){
-    iplweb.reciprocity.pushstream = sinon.createStubInstance(PushStream, {
-	connect: sinon.stub()
-    });
+QUnit.test('reciprocity.connect', function (assert) {
     iplweb.reciprocity.connect();
     assert.equal(iplweb.reciprocity.pushstream.connect.called, true);
 });
 
-QUnit.test('check onError', function(assert) {
-    assert.equal(iplweb.reciprocity.loadingFailed, false);
-    
+QUnit.test('reciprocity.onError', function (assert) {
+    assert.notOk(iplweb.reciprocity.loadingFailed);
     iplweb.reciprocity.onError({type: "load"});
-    assert.equal(jQuery.fn.loadTemplate.called, true);
-    
-    assert.equal(iplweb.reciprocity.loadingFailed, true);
+    assert.ok(jQuery.fn.loadTemplate.called);
+    assert.ok(iplweb.reciprocity.loadingFailed);
 });
 
+QUnit.test('reciprocity.subscribe', function (assert) {
+    iplweb.reciprocity.subscribe('foo');
+    assert.ok(iplweb.reciprocity.pushstream.addChannel.called);
+});
+
+QUnit.test('reciprocity.callout', function (assert) {
+    iplweb.reciprocity.callout({
+        "class": "success",
+        "header": "header",
+        "body": "body"
+    });
+    assert.ok(jQuery.fn.loadTemplate.called);
+});
+
+
+QUnit.test('reciprocity.calloutWithLink', function (assert) {
+    iplweb.reciprocity.calloutWithLink({
+        "class": "success",
+        "header": "header",
+        "body": "body",
+        "href": "lel"
+    });
+    assert.ok(jQuery.fn.loadTemplate.called);
+});
